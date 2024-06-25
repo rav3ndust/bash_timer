53178a4e1f3ab70442d5f81292b9c1ac71a436d7
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2024-06-25 18:15:24 -0500

    Create timer.sh

---
+++ timer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+title="Bash Timer"
+prompt="How long do you want to set a timer for?"
+alert="Your timer is up!"
+alert_func () {
+	# alerts user
+	# uses `sleep` cmd.
+	# set timer:
+	# seconds 's' (default)
+	# minutes 'm' (ex: 5m)
+	# hours 'h' (ex: 2h 30m)
+	echo "${alert}"; espeak "${alert}"
+	notify-send "${title}" "${alert}"
+}
+main () {
+	echo "${prompt}"; read input_time
+	local start_time=$(date +%r)
+	echo "Timer started at ${start_time}."
+	sleep ${input_time}
+	alert_func
+	local finish_time=$(date +%r)
+	echo "Timer finished at ${finish_time}."
+	exit
+}
+# - - - entry
+main
